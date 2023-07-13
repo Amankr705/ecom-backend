aec0be1082c928f688fab474f88a4bec3fc87947
Amankr705 <ak.1000aman@gmail.com>
2023-07-14 02:09:18 +0530

    ft: auth routes & tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,91 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const { expressjwt: expressJwt } = require('express-jwt');
 
-exports.signup = (req, res) => {
-  const errors = validationResult(req);
+exports.signup = async (req, res) => {
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ erros: errors.array()[0].msg });
-  }
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array()[0].msg });
+    }
 
-  const user = new User(req.body);
-  user
-    .save()
-    .then((user) => {
-      res.json({
-        name: user.name,
-        email: user.email,
-        id: user._id,
-      });
-    })
-    .catch((err) => {
-      res.status(400).json({
-        err: 'Failed to save',
-      });
+    const user = new User(req.body);
+    const savedUser = await user.save();
+
+    return res.json({
+      name: savedUser.name,
+      email: savedUser.email,
+      id: savedUser._id,
     });
+  } catch (error) {
+    return res.status(400).json({
+      error: 'Failed to save user',
+    });
+  }
+};
+
+exports.signin = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    const { email, password } = req.body;
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array()[0].msg });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user || !user.authenticate(password)) {
+      return res.status(400).json({ error: 'User Credentials are not valid' });
+    }
+
+    //create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+    //put token in cookie
+    res.cookie('token', token, { expire: new Date() + 9999 });
+
+    //send response too frontend
+    const { _id, name, role } = user;
+    const userEmail = user.email;
+    return res.json({ token, user: { _id, name, email: userEmail, role } });
+  } catch (error) {
+    return res.status(400).json({ error: 'An error occured' });
+  }
 };
 
 exports.signout = (req, res) => {
-  return res.send('user signout success');
+  res.clearCookie('token');
+  res.json({ message: 'User signout successfully' });
+};
+
+//protected routes  (checker for token)
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  algorithms: ['HS256'],
+  userProperty: 'auth',
+});
+
+//custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+  const { profile, auth } = req;
+  let checker = profile && auth && profile._id === auth._id;
+
+  if (!checker) {
+    return res.status(403).json({ error: 'ACCESS DENIED' });
+  }
+
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  const { profile } = req;
+
+  if(profile.role === 0){
+    return res.status(403).json({ error: 'NOT ADMIN ACCESS' });
+  }
+
+  next();
 };
--- routes/auth.js
+++ routes/auth.js
@@ -9,11 +9,25 @@ router.post(
   [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid unique email').isEmail(),
-    body('password', 'Password must be atleast 5 characters').isLength({min: 5}),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
   ],
   controller.signup
 );
 
+router.post(
+  '/signin',
+  [
+    body('email', 'Enter a valid unique email').isEmail(),
+    body('password', 'Password is required').isLength({ min: 1 }),
+  ],
+  controller.signin
+);
+
 router.get('/signout', controller.signout);
+
+// router.get('/testroute', controller.isSignedIn, (req, res)=>{
+//   res.send('Protected Route')
+//   res.json(req.auth)
+// })
 
 module.exports = router;
